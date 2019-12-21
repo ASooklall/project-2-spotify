f99dcf9c8e64fd8dc1528eb474c56bee2dea2c03
Andrew Sooklall <andrewsooklall@gmail.com>
2019-12-21 15:05:23 -0500

    updated table script with init and start of filter

--- static/js/table.js
+++ static/js/table.js
@@ -9,7 +9,7 @@
 ///////////////////////////////////
 
 var dataURL = '/top_data'
-var tbody = d3.select(".test");
+var tbody = d3.select("tbody");
 
 
 
@@ -17,107 +17,146 @@ var tbody = d3.select(".test");
 // tbody.append('p').html("test");
 // tbody.append('p').html(dataURL);
 
-// test to pull data
-d3.json(dataURL, data => {
-    data.forEach(song =>{
-        tbody.append('p').html(song.artists)
-    })
-    console.log(data[0])
-})
+// // test to pull data
+// d3.json(dataURL, data => {
+//     // data.forEach(song =>{
+//     //     tbody.append('p').html(song.artists)
+//     // })
+//     // tbody.append('p').html(data[0].artists)
+//     console.log(data.filter(data => data.artists === 'Ed Sheeran'))
+//     test2 = data.filter(data =>data.artists === 'Ed Sheeran')
+//     test3 = test2.forEach((song) => {
+//         Object.entries(song).forEach(([key,value]) => {
+//             console.log(value)
 
-  // append html to table after search filters
-//   filteredShape.forEach((ufoSighting) => {
-//     var row = tbody.append("tr");
-//     Object.entries(ufoSighting).forEach(([key, value]) => {
-//       var cell = row.append("td");
-//       cell.text(value);
+//         })
+//         console.log('-------')
 //     })
-// });
+// })
 
-// ///////////////////////////////////
-// ///////// Filter Function /////////
-// ///////////////////////////////////
+///////////////////////////////////
+//////// Initialize Table /////////
+///////////////////////////////////
 
-// function handleSubmit() {
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
-
-//   // Select the new input values from the form
-//   var inputValue = d3.select("#datetime").property("value");
-//   var inputCity = d3.select("#cities").property("value").toLowerCase();
-//   var inputState = d3.select("#states").property("value").toLowerCase();
-//   var inputCountry = d3.select("#countries").property("value").toLowerCase();
-//   var inputShape = d3.select("#shapes").property("value").toLowerCase();
-
-//   // clear the table to prepare for new data filter
-//   tbody.html("")
-
-//   // Begin filters 
-//     // filters by each input which may or may not have values
-
-//     // filter check based on date
-//     if (inputValue){
-//       var filteredDate = tableData.filter(data => data.datetime === inputValue)
-//     }
-//     else {
-//       var filteredDate = tableData.filter(data => data.datetime)
-//     };
-//     // then filter check for city
-//     if (inputCity){
-//       var filteredCity = filteredDate.filter(data => data.city === inputCity)
-//     }
-//     else {
-//       var filteredCity = filteredDate .filter(data => data.datetime)
-//     };
-//     // then filter check for state
-//     if (inputState){
-//       var filteredState = filteredCity.filter(data => data.state === inputState)
-//     }
-//     else {
-//       var filteredState = filteredCity.filter(data => data.state)
-
-//     };
-//     // then filter check for country
-//     if (inputCountry){
-//         var filteredCountry = filteredState.filter(data => data.country === inputCountry)
-//     }
-//     else {
-//       var filteredCountry = filteredState.filter(data => data.country)
-//     };
-//     // then filter check for shape
-//     if (inputShape){
-//         var filteredShape = filteredCountry.filter(data => data.shape === inputShape)
-//     }
-//     else {
-//       var filteredShape = filteredCountry.filter(data => data.shape)
-//     };
+// test
+d3.json(dataURL, data => {
+    test2 = data.filter(data => data.name === 'Shape of You')
+    test3 = test2[0].acousticness
+    test4 = typeof test3
+    console.log(test3)
+    console.log(test4)
+});
 
 
-//   // append html to table after search filters
-//   filteredShape.forEach((ufoSighting) => {
-//       var row = tbody.append("tr");
-//       Object.entries(ufoSighting).forEach(([key, value]) => {
-//         var cell = row.append("td");
-//         cell.text(value);
-//       })
-//   });
-      
+// initialize table with data
+function init() {
+    d3.json(dataURL, data => {
+    // d3.event.preventDefault();
+        data.forEach((song) => {
 
 
-//   // clear search inputs
-//   d3.select("#datetime").node().value = "";
-//   d3.select("#cities").node().value = "";
-//   d3.select("#states").node().value = "";
-//   d3.select("#countries").node().value = "";
-//   d3.select("#shapes").node().value = "";
-// };
+            var row = tbody.append("tr");
+            Object.entries(song).forEach(([key, value]) => {
+                var cell = row.append("td");
+                if (typeof value == 'number'){
+                    cell.text(value.toFixed(5))
+                }
+                else {
+                    cell.text(value);
+                }
+            })
+            })
+    })
+};
+
+// call init
+init()
+
+///////////////////////////////////
+///////// Filter Function /////////
+///////////////////////////////////
+
+function handleSubmit() {
+    d3.json(dataURL, data => {
+        console.log(data)
+        // Prevent the page from refreshing
+        d3.event.preventDefault();
+
+        // Select the new input values from the form
+        var inputSong = d3.select("#songs").property("value");
+        var inputArtist = d3.select("#artists").property("value");
+        var inputGenre = d3.select("#genres").property("value");
+        var inputYear = d3.select("#years").property("value");
+
+        // clear the table to prepare for new data filter
+        tbody.html("")
+
+        console.log(data.filter(data => data.name === 'Shape of You'))
+        console.log(data.filter(data => data.name === inputSong))
+
+        // Begin filters 
+            // filters by each input which may or may not have values
+
+            // filter check based on date
+            if (inputSong){
+            var filteredSong = data.filter(data => data.name === inputSong)
+            console.log(inputSong)
+            console.log(filteredSong)
+            }
+            else {
+            var filteredSong = data.filter(data => data.name)
+            };
+            // then filter check for city
+            if (inputArtist){
+            var filteredArtist = filteredSong.filter(data => data.artists === inputArtist)
+            }
+            else {
+            var filteredArtist = filteredSong.filter(data => data.artists)
+            };
+            // then filter check for state
+            if (inputGenre){
+            var filteredGenre = filteredArtist.filter(data => data.genre === inputGenre)
+            }
+            else {
+            var filteredGenre = filteredArtist.filter(data => data.genre)
+
+            };
+            // then filter check for country
+            if (inputYear){
+                var filteredYear = filteredGenre.filter(data => data.year === inputYear)
+                console.log(inputYear)
+            }
+            else {
+            var filteredYear = filteredGenre.filter(data => data.year)
+            };
+
+    console.log(filteredYear)
+
+        // append html to table after search filters
+        filteredYear.forEach((song) => {
+            var row = tbody.append("tr");
+            Object.entries(song).forEach(([key, value]) => {
+                var cell = row.append("td");
+                // cell.html(`<p>${value}</p>`);
+                // cell.text(value);
+                cell.append("p").html(value);
+            })
+        });
+
+        // clear search inputs
+        d3.select("#songs").node().value = "";
+        d3.select("#artists").node().value = "";
+        d3.select("#genres").node().value = "";
+        d3.select("#years").node().value = "";
+    })
+};
     
-// ///////////////////////////
-// ///// Event Listeners /////
-// ///////////////////////////
+///////////////////////////
+///// Event Listeners /////
+///////////////////////////
 
-// d3.select("#filter-btn").on("click", handleSubmit);
-// d3.select("form").on("submit", handleSubmit);
+d3.select("#filter-btn").on("click", handleSubmit);
+d3.select("form").on("submit", handleSubmit);
 
 
 
