c783cb76f8b7b43bdd012e2147e5b76801e6e9be
Mikey Esteban <michaelp.esteban@gmail.com>
2019-12-21 15:20:02 -0500

    refactored scrape to add 2017/2018 genres

--- scrape.py
+++ scrape.py
@@ -11,7 +11,10 @@ def run_scrape():
     ########################
     ### grab old csv file #
     ######################
+    top_2017 = pd.read_csv("assets/data/top2017.csv", encoding='latin-1')
+    top_2018 = pd.read_csv("assets/data/top2018.csv", encoding='unicode_escape')
     top_2019 = pd.read_csv("assets/data/top2019.csv", encoding='unicode_escape')
+
 
     # initialize list to scrape
     search_data = []
@@ -67,6 +70,19 @@ def run_scrape():
                 track = possible_song
                 return track
 
+    def grab_genre(artist):
+        ''' returns a genre of artist'''
+        # create spotipy object
+        sp = spotipy.Spotify(auth=token)
+
+        # search by artist name
+        result = sp.search(q=artist, type='artist')
+        # grab artist id
+        artist_id = result['artists']['items'][0]['id']
+        # search artist top tracks
+        genre = sp.artist(artist_id)['genres']
+        return genre
+
 
     ############################################
     ### Spotipy Scrape for results obeject  ###
@@ -84,6 +100,7 @@ def run_scrape():
             # create track & artist search variables
             track = query[0]
             artist = query[1]
+            genre = grab_genre(artist)
 
             # run search by track to find results
             value = search_by_track(track, artist)
@@ -94,8 +111,10 @@ def run_scrape():
                 correct_track = search_by_artist(artist, track)
                 # run search_by_track again
                 value = search_by_track(correct_track, artist)
+                value.append(genre)
                 results.append(value)
             else:
+                value.append(genre)
                 results.append(value)
         else:
             print("Can't get token for", username)
@@ -117,6 +136,7 @@ def run_scrape():
         data['name'] = each[1]
         data['artists'] = each[0]
         data['year'] = '2019'
+        data['genre'] = each[3]
         top_2019_data.append(data)
 
 
@@ -129,11 +149,60 @@ def run_scrape():
     # Drop unneeded columns
     top_2019_df = top2019_df.drop(columns=['type', 'uri','track_href','analysis_url'])
     # Reorder columns
-    top_2019_df = top_2019_df[['id','name','artists','danceability','energy','key','loudness','mode','speechiness','acousticness','instrumentalness','liveness','valence','tempo','duration_ms','time_signature','year']]
+    top_2019_df = top_2019_df[['id','name','artists','danceability','energy','key','loudness','mode','speechiness','acousticness','instrumentalness','liveness','valence','tempo','duration_ms','time_signature','year','genre']]
+
+
+    ##########################################
+    ### add genre to top2017 & top2018 csv ##
+    ########################################
+    def add_genre(dataframe):
+        '''takes in a dataframe, adds a genre column by artist'''
+        token = util.prompt_for_user_token(username, scope, client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
+
+        # grab artist column for Spotipy search
+        artists = list(dataframe['artists'])
+
+        # initialize genre column
+        genres = []
+
+        for person in artists:
+            # create spotipy object
+            sp = spotipy.Spotify(auth=token)
+            # search by artist name
+            result = sp.search(q=person, type='artist')
+            try:
+                # grab artist id
+                artist_id = result['artists']['items'][0]['id']
+                # search artist top tracks
+                genre = sp.artist(artist_id)['genres']
+                genres.append(genre)
+            except IndexError:
+                i = dataframe.index[dataframe['artists'] == person]
+                song_df = dataframe.loc[i, 'name']
+                song_list = song_df.values
+                song = song_list[0]
+                sp = spotipy.Spotify(auth=token)
+                result = sp.search(q=song, type='track')
+                artist = result['tracks']['items'][0]['artists'][0]['name']
+                sp = spotipy.Spotify(auth=token)
+                result = sp.search(q=artist, type='artist')
+                genre = result['artists']['items'][0]['genres']
+                genres.append(genre)
+
+        dataframe['genre'] = genres
+
+        return dataframe
+
+    # Add to each df
+    top_2017_df = add_genre(top_2017)
+    top_2018_df = add_genre(top_2018)
+
 
     #########################
-    ## save DF as excel ####
+    ## save DFs as excel ####
     #######################
+    top_2017_df.to_excel('assets/data/top2017.xlsx', index=False)
+    top_2018_df.to_excel('assets/data/top2018.xlsx', index=False)
     top_2019_df.to_excel('assets/data/top2019.xlsx', index=False)
 
 if __name__ == "__main__":
