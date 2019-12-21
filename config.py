e9b855b080d39d9cfa74c70ca2f0d34c2bf420e1
Andrew Sooklall <andrewsooklall@gmail.com>
2019-12-21 12:06:26 -0500

    added gitignore

--- config.py
+++
@@ -1,7 +0,0 @@
-#Spotipy config
-SPOTIPY_CLIENT_ID='7655188a18144343b07e44965cc38310'
-SPOTIPY_CLIENT_SECRET='2bec9b47853e4ea3a419418a211ec45d'
-SPOTIPY_REDIRECT_URI='https://google.com/'
-scope = 'user-library-read'
-# Grab username
-username = "1298997744"
