0203188ebf5b5451b5f9b86d261d96439ae1b7a1
zclynchzc <zclynchzc@gmail.com>
2019-12-21 13:24:26 -0500

    get table and column names from sqlite in app.py

--- app.py
+++ app.py
@@ -55,28 +55,39 @@ def index():
 @app.route("/top_data")
 def top_data():
     global engine
+    global inspector
+
     songs = []
-    column_names = ["index",
-                    "id",
-                    "name",
-                    "artist",
-                    "danceability",
-                    "energy",
-                    "key",
-                    "loudness",
-                    "mode",
-                    "speechiness",
-                    "acousticness",
-                    "instrumentalness",
-                    "liveness",
-                    "valence",
-                    "tempo",
-                    "duration_ms",
-                    "time_signature",
-                    "genre",
-                    "year"]
+    column_names = []
+
+    table_name = inspector.get_table_names()[0]
+    table_columns = inspector.get_columns(table_name)
+    for col in table_columns:
+        # print(col['name'])
+        column_names.append(col['name'])
+
+    # column_names = ["index",
+    #                 "id",
+    #                 "name",
+    #                 "artist",
+    #                 "danceability",
+    #                 "energy",
+    #                 "key",
+    #                 "loudness",
+    #                 "mode",
+    #                 "speechiness",
+    #                 "acousticness",
+    #                 "instrumentalness",
+    #                 "liveness",
+    #                 "valence",
+    #                 "tempo",
+    #                 "duration_ms",
+    #                 "time_signature",
+    #                 "genre",
+    #                 "year"]
+
     # with engine.connect() as con:
-    data = engine.execute('SELECT * FROM spotify')
+    data = engine.execute('SELECT * FROM ' + table_name)
 
     for row in data:
         temp = {}
